7dbdf0c3bc13210bbcce7497dc67a3707367b105
moise <moise@MOY>
2024-01-03 15:26:08 -0600

    The RutaCarpeta class has been created to better handle the main folder's path

---
+++ GetDataTXT/RutaCarpeta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDataTXT
+{
+    internal class RutaCarpeta
+    {
+        //Variables
+        private string carpetaRuta;
+
+        // Propiedad para obtener la ruta de la carpeta
+        public string GetRuta
+        {
+            get { return carpetaRuta; }
+        }
+
+        // Método para establecer la ruta de la carpeta
+        public void SetRuta(string nuevaRuta)
+        {
+            carpetaRuta = nuevaRuta;
+        }
+
+        // Método para imprimir información sobre la carpeta
+        public string ShowRuta()
+        {
+            return "Ruta de la carpeta: " + carpetaRuta;
+        }
+    }
+}
